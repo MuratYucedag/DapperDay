7a909f37ce7892865271cb25372def5446abc0f8
MuratYucedag <yucedagmurat23@gmail.com>
2024-06-27 15:38:22 +0300

    ...

--- DapperDay/Program.cs
+++ DapperDay/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<DapperContext>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IProductService, ProductService>();
 
 builder.Services.AddControllersWithViews();
 
